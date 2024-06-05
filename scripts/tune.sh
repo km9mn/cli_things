11995af4ae1be68388f05742c07733b7b9331a6a
km9mn <rrakim31@korea.ac.kr>
2024-06-05 15:21:48 +0000

    change tune.sh to run bash but with cd now

--- scripts/tune.sh
+++ scripts/tune.sh
@@ -35,7 +35,10 @@ echo "conda activate tune"
 conda activate tune
 
 SAMPLES_DIR=$HOME/project/SAIT_project/src/external/torchtune/recipes/scripts
-tune $SAMPLES_DIR/run --nproc_per_node 2 lora_finetune_distributed --config recipes/configs/llama3/cluster_8B_lora_kernel_fixed_fair_and_balanced_dulda_true.yaml
+cd $SAMPLES_DIR
+bash cluster_tune_lora_kernel_filtered_123_dulda.sh
+
+cd $SLURM_SUBMIT_DIR
 
 date
 
