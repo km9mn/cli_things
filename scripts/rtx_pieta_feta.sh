4082fb11dd72de3e1d3734ebd0002f14d16c24a3
km9mn <kyuminkim@postech.ac.kr>
2025-04-01 11:32:51 +0000

    add fetaqa tuning script

---
+++ scripts/rtx_pieta_feta.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+#SBATCH -J rtx_pieta_feta
+#SBATCH -o rtx_pieta_feta.out
+#SBATCH -t 72:00:00
+
+#### Select GPU
+#SBATCH -p RTX6000ADA
+#SBATCH --gres=gpu:4
+
+## node 지정하기
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+cd  $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+echo "Start"
+echo "conda PATH"
+
+echo "source $HOME/anaconda3/etc/profile.d/conda.sh"
+source $HOME/anaconda3/etc/profile.d/conda.sh
+
+echo "conda activate tune"
+conda activate tune
+
+SAMPLES_DIR=$HOME/project/torchtune
+cd $SAMPLES_DIR
+
+tune run --nproc_per_node 4 --master_port 29600 full_finetune_distributed --config recipes/configs/llama3_1/fetaqa/8B_full.yaml
+
+date
+
+echo "conda deactivate"
+
+conda deactivate
+
+squeue --job $SLURM_JOBID
+
+echo "##### END #####"
