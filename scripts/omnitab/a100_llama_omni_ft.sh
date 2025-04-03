a6660a296e2ba789cd95875bc1d2574a8f095eee
km9mn <kyuminkim@postech.ac.kr>
2025-04-03 13:40:21 +0000

    add a100 script

---
+++ scripts/omnitab/a100_llama_omni_ft.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+#SBATCH -J wtq_llama
+#SBATCH -o wtq_llama.out
+#SBATCH -t 72:00:00
+
+#### Select GPU
+#SBATCH -q hpgpu
+#SBATCH -p A100-80GB
+#SBATCH --gres=gpu:4
+
+## node 지정하기
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+cd  $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+echo "Start"
+echo "conda PATH"
+
+echo "source $HOME/anaconda3/etc/profile.d/conda.sh"
+source $HOME/anaconda3/etc/profile.d/conda.sh
+
+echo "conda activate tune"
+conda activate tune
+
+SAMPLES_DIR=$HOME/project/torchtune
+cd $SAMPLES_DIR
+
+tune run --nproc_per_node 4 --master_port 29600 full_finetune_distributed --config recipes/configs/llama3_1/omnitab/8B_full_wtq_cluster_mem.yaml
+
+date
+
+echo "conda deactivate"
+
+conda deactivate
+
+squeue --job $SLURM_JOBID
+
+echo "##### END #####"
