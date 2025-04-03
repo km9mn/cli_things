9cec38ec1e2880c05d97bea9999281561fdc147c
km9mn <kyuminkim@postech.ac.kr>
2025-04-03 13:54:05 +0000

    ada 6 gpu

---
+++ scripts/omnitab/ada_6_llama_omni_ft.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+#SBATCH -J rtx_wtq_llama
+#SBATCH -o rtx_wtq_llama.out
+#SBATCH -t 72:00:00
+
+#### Select GPU
+#SBATCH -p RTX6000ADA
+#SBATCH --gres=gpu:6
+
+## node 지정하기
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+
+cd  $SLURM_SUBMIT_DIR
+
+echo "SLURM_SUBMIT_DIR=$SLURM_SUBMIT_DIR"
+echo "CUDA_HOME=$CUDA_HOME"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "CUDA_VERSION=$CUDA_VERSION"
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+echo "Start"
+echo "conda PATH"
+
+echo "source $HOME/anaconda3/etc/profile.d/conda.sh"
+source $HOME/anaconda3/etc/profile.d/conda.sh
+
+echo "conda activate tune"
+conda activate tune
+
+SAMPLES_DIR=$HOME/project/torchtune
+cd $SAMPLES_DIR
+
+tune run --nproc_per_node 6 --master_port 29600 full_finetune_distributed --config recipes/configs/llama3_1/omnitab/8B_full_wtq_cluster.yaml
+
+date
+
+echo "conda deactivate"
+
+conda deactivate
+
+squeue --job $SLURM_JOBID
+
+echo "##### END #####"
